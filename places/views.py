3ddf8e70a7a12bc63f04b0a6354d357ed9ef99b1
Jinny Wong <jinny.wong@xseedsolutions.com>
2013-07-09 12:51:01 +0800

    added random tip to a place (if a tip/reason exists)

--- places/views.py
+++ places/views.py
@@ -7,14 +7,19 @@ from django.http import HttpResponse
 
 def index(request):
   # ll=3.102222,101.5994444
-  uri = "https://api.foursquare.com/v2/venues/explore?sw=3.10027,101.59768&ne=3.10698,101.60011&section=food&limit=50&venuePhotos=1&query=food&limit=30&client_id=AHRI2C3I0GXB1MNWI5OSWPBDWH222VNJERYH0PZCNK0PMJOF&client_secret=YLODU3QTTL5FX5TMVC4FQASAKM5LCGUDAKUZVKKAP1H0TH5G&v=20130605"
+  uri = "https://api.foursquare.com/v2/venues/explore?sw=3.10027,101.59768&ne=3.10698,101.60011&section=food&limit=50&venuePhotos=1&query=food&limit=50&client_id=AHRI2C3I0GXB1MNWI5OSWPBDWH222VNJERYH0PZCNK0PMJOF&client_secret=YLODU3QTTL5FX5TMVC4FQASAKM5LCGUDAKUZVKKAP1H0TH5G&v=20130605"
   # 
   req = requests.get(uri)
   all_places = req.json()["response"]["groups"][0]["items"]
   selected_place = random.choice(all_places)
   print selected_place
+  if 'phrases' in selected_place:
+    tips = random.choice(selected_place["phrases"])
+  else:
+    tips = None
   t = loader.get_template('places/index.html')
   c = Context({
     'selected_place': selected_place,
+    'tips': tips,
   })
   return HttpResponse(t.render(c))
